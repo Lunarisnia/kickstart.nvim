22ea77b35a3932ecfc8a4e514ec0415cc495bfa1
louna <r.arswendo.r@gmail.com>
2024-09-10 16:56:16 +0700

    change barbar setting and clipboard

--- lua/custom/plugins/barbar.lua
+++ lua/custom/plugins/barbar.lua
@@ -21,7 +21,7 @@ return {
   end,
   opts = {
     animation = true,
-    insert_at_start = true,
+    insert_at_start = false,
   },
   version = '^1.0.0', -- optional: only update when a new 1.x version is released
 }
