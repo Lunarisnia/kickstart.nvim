8ac60d7989338c02cddf148f190a250191ddbe33
Lounarisnia <r.arswendo.r@gmail.com>
2025-04-18 21:27:00 +0700

    feat: added debugger

--- init.lua
+++ init.lua
@@ -540,6 +540,14 @@ require('lazy').setup({
             vim.cmd(replace_command)
           end, 'Global Rename')
 
+          -- nvim dap mappings -----
+          map('<leader>db', function()
+            vim.cmd 'DapToggleBreakpoint'
+          end, 'Add breakpoint at a line')
+          map('<leader>dr', function()
+            vim.cmd 'DapContinue'
+          end, 'Start or continue the debugger')
+
           -- Jump to the definition of the word under your cursor.
           --  This is where a variable was first declared, or where a function is defined, etc.
           --  To jump back, press <C-t>.
@@ -635,7 +643,6 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
         gopls = {},
         pyright = {},
         -- rust_analyzer = {},
@@ -696,6 +703,7 @@ require('lazy').setup({
         -- C++
         'clangd',
         'clang-format',
+        'codelldb',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
